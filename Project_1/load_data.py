19757a2a9d4c95aefb6d47393be9ff49b64dc1d0
Rudi <k.r.siebert@icloud.com>
2022-11-23 21:04:08 +0100

    some function testing

---
+++ Project_1/load_data.py
@@ -0,0 +1,40 @@
+
+import torch
+from torchvision import datasets
+
+import os
+
+
+######################################################################
+#   functions from dlc_practical_prologue
+######################################################################
+
+def mnist_to_pairs(nb, input, target):
+    input = torch.functional.F.avg_pool2d(input, kernel_size = 2)
+    a = torch.randperm(input.size(0))
+    a = a[:2 * nb].view(nb, 2)
+    input = torch.cat((input[a[:, 0]], input[a[:, 1]]), 1)
+    classes = target[a]
+    target = (classes[:, 0] <= classes[:, 1]).long()
+    return input, target, classes
+
+def generate_pair_sets(nb):
+    if args.data_dir is not None:
+        data_dir = args.data_dir
+    else:
+        data_dir = os.environ.get('PYTORCH_DATA_DIR')
+        if data_dir is None:
+            data_dir = './data'
+
+    train_set = datasets.MNIST(data_dir + '/mnist/', train = True, download = True)
+    train_input = train_set.data.view(-1, 1, 28, 28).float()
+    train_target = train_set.targets
+
+    test_set = datasets.MNIST(data_dir + '/mnist/', train = False, download = True)
+    test_input = test_set.data.view(-1, 1, 28, 28).float()
+    test_target = test_set.targets
+
+    return mnist_to_pairs(nb, train_input, train_target) + \
+           mnist_to_pairs(nb, test_input, test_target)
+
+######################################################################
